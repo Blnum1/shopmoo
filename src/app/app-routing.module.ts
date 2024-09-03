0c782dddbf563c23a4dcc4fb346c839022915596
Blnum1 <jirayut.pr@ku.th>
2024-09-03 22:14:01 +0700

    blue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductComponent } from './product/product.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PriceComponent } from './price/price.component';
+import { ShipmentComponent } from './shipment/shipment.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'product', component: ProductComponent },
+  { path: 'about-us', component: AboutUsComponent }, 
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'price', component: PriceComponent },
+  { path: 'shipment', component: ShipmentComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductComponent } from './product/product.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PriceComponent } from './price/price.component';
+import { ShipmentComponent } from './shipment/shipment.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ProductComponent,
+    ContactUsComponent,
+    PriceComponent,
+    ShipmentComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
